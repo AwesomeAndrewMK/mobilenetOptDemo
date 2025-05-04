ac31d8e3c69a0c6bd8175df300e78d7c388df4e8
Andrii Kuznietsov <awesomeakm@gmail.com>
2025-05-04 21:12:24 +0300

    mobilenetv2 model run set up

--- App.tsx
+++ App.tsx
@@ -1,131 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Text, SafeAreaView, StyleSheet, TouchableOpacity, Alert} from 'react-native';
+import {loadTensorflowModel, TensorflowModel} from 'react-native-fast-tflite';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+export const App = () => {
+  const mobilenetv2_fp32 = loadTensorflowModel(
+    require('./models/mobilenetv2_fp32.tflite'),
   );
-}
+  const mobilenetv2_fp16 = loadTensorflowModel(
+    require('./models/mobilenetv2_fp16.tflite'),
+  );
+  const mobilenetv2_int8 = loadTensorflowModel(
+    require('./models/mobilenetv2_int8.tflite'),
+  );
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const input = new Float32Array(224 * 224 * 3).fill(0.5);
+  const inputINT8 = new Uint8Array(224 * 224 * 3).fill(0.5);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const loadModel = async (
+    mobilenetv2Model: Promise<TensorflowModel>,
+    inputItem: Float32Array | Uint8Array,
+    label: string,
+  ) => {
+    try {
+      const model = await mobilenetv2Model;
+      const start = Date.now();
+      await model.run([inputItem]);
+      const end = Date.now();
+      console.log(`${label} model inference time: ${end - start} ms`);
+      Alert.alert(`${label} model inference time: ${end - start} ms`);
+    } catch (error) {
+      console.error('Error loading model:', error);
+    }
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
-
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
+    <SafeAreaView style={styles.center}>
+      <TouchableOpacity>
+        <Text
+          style={styles.button}
+          onPress={async () => {
+            await loadModel(mobilenetv2_fp32, input, 'FP32');
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </View>
+          Load mobilenetv2_fp32
+        </Text>
+        <Text
+          style={styles.button}
+          onPress={async () => {
+            await loadModel(mobilenetv2_fp16, input, 'FP16');
+          }}>
+          Load mobilenetv2_fp16
+        </Text>
+        <Text
+          onPress={async () => {
+            await loadModel(mobilenetv2_int8, inputINT8, 'INT8');
+          }}>
+          Load mobilenetv2_int8
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    paddingBottom: 16,
   },
 });
-
-export default App;
